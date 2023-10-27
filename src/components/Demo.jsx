babe2d34f5240363ed92e492a9c8a6c761b507a7
SeniboDonPedro <senibodonpedro@gmail.com>
2023-10-27 23:39:21 +0100

    done with the api

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import {copy, linkIcon, loader, tick} from '../assets'
+import { AiOutlineLink } from "react-icons/ai";
+import { AiOutlineEnter } from "react-icons/ai";
+import { useLazyGetSummaryQuery } from "../services/article";
+
 
 const Demo = () => {
+
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const {data} = await getSummary({ articleUrl: article.url });
+
+    if(data?.summary) {
+      const newArticle = {...article, summary:data.summary};
+    }
+  }
+
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form 
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <AiOutlineLink 
+            className="absolute left-0 my-2 ml-3 w-5" 
+          />
+          <input 
+            type="url" 
+            placeholder="Enter a URL" 
+            value={article.url}
+            onChange={(e) => setArticle({
+              ...article, url: e.target.value
+            })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"  
+            className="submit_btn peer-focus:border-gray-500 peer-focus:text-gray-500"
+          >
+            <AiOutlineEnter />
+          </button>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
